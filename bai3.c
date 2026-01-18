4b7753add1e5a24352f51d86dcc230c30a3d39c2
vodat0971312100 <vodat0971312100@gmail.com>
2026-01-18 19:05:45 +0700

    nguyễn võ minh đạt nập bài lap 3

--- bai3.c
+++ bai3.c
@@ -1,7 +1,7 @@
 /******************************************************************************
- * Họ và tên: [ĐIỀN TÊN TẠI ĐÂY]
- * MSSV:      [ĐIỀN MSSV TẠI ĐÂY]
- * Lớp:       [ĐIỀN LỚP TẠI ĐÂY]
+ * Họ và tên: [Nguyễn võ minh đạt ]
+ * MSSV:      [PS49299]
+ * Lớp:       [COM108]
  *****************************************************************************/
 
 //  BÀI 3: XÂY DỰNG CHƯƠNG TRÌNH TÍNH TIỀN ĐIỆN 
@@ -12,7 +12,34 @@
 
 int main(){
     
-    // Khai báo biến
+    float kWh;
+    printf("số điện tiêu thụ 1 tháng là :");
+    scanf("%f",&kWh);
+    if(kWh<=600 && kWh>0){
+        if(kWh > 401){
+            printf("giá cho một kí điện bậc 6 là 3.460 đồng/kWh =%.3f",kWh*3.460);
+        }
+        else if(kWh>=301){
+           printf("giá cho một kí điện bậc 5 là 3.350 đồng/kWh =%.3f",kWh*3.350);
+        }
+        else if(kWh>= 201 ){
+            printf("giá cho một kí điện bậc 4 là 2.998 đồng/kWh =%.3f",kWh*2.998);
+        }
+        else if (kWh >= 101){
+            printf("giá cho một kí điện bậc 3 là 2.380 đồng/kWh =%.3f",kWh*2.380);
+        }
+        else if (kWh>=51){
+            printf("giá cho một kí điện bậc 2 là 2.050 đồng/kWh =%.3f",kWh*2.050);
+        }
+        else{
+            printf("giá cho một kí điện bậc 1 là 1.984 đồng/kWh =%.3f",kWh*1.984);
+        }
+    }
+    else{
+        printf("số kí điện không hợp lệ! \nvui lòng nhập lại ");
+    }
+    return 0;
+    
 
 
     // Nhập dữ liệu
--- bai4.c
+++ bai4.c
@@ -1,7 +1,7 @@
 /******************************************************************************
- * Họ và tên: [ĐIỀN TÊN TẠI ĐÂY]
- * MSSV:      [ĐIỀN MSSV TẠI ĐÂY]
- * Lớp:       [ĐIỀN LỚP TẠI ĐÂY]
+ * Họ và tên: [Nguyễn Võ Minh Đạt]
+ * MSSV:      [PS49299]
+ * Lớp:       [COM108
  *****************************************************************************/
 
 //  BÀI 4: XÂY DỰNG MENU CHƯƠNG TRÌNH CHO 3 BÀI TẬP TRÊN 
@@ -10,8 +10,50 @@
 #include <stdio.h>
 
 int main(){
+    printf("số tiền phải trả cho lượng điện tiêu thụ là 400 trên thang là bậc thứ mấy\n");
+    printf("a.5\nb.2\nc.1\nd.6\n");
+    printf("hãy chọn 1 trong 4 đáp án trên\n");
+    char dapAn;
+    scanf(" %s",&dapAn);
+    switch (dapAn){
+         case 'a': printf(" đáp án chính xắc!\n");
+         break;
+         case 'b': printf("sai rồi huhu!\n");
+         break;
+         case 'c':printf("sai rồi huhu!\n");
+         break;
+         case 'd':printf("sai rồi hihi!\n");
+    }
+    printf("5x2 – 7x = 0 phương trình bậc 2 này sẽ có những nghiệm nà0:\n");
+    printf("a.x=0,x=7/5\nb.x=2,x=6/7\nc.x=1,x=7/8\nd.x=6,x=0\n");
+    printf("hãy chọn 1 trong 4 đáp án trên\n");
+    char Dapan;
+    scanf(" %s",&Dapan);
+    switch (Dapan){
+         case 'a': printf(" đáp án chính xắc!\n");
+         break;
+         case 'b': printf("sai rồi huhu!\n");
+         break;
+         case 'c':printf("sai rồi huhu!\n");
+         break;
+         case 'd':printf("sai rồi hihi!\n");
+    }
+     printf("7x-35 = 0 phương trình bậc 1 này sẽ có những nghiệm nà0:\n");
+    printf("a.x=5,\nb.x=2\nc.phương ttrình vô nghiệm\nd.phương trình vô số nghiệm\n");
+    printf("hãy chọn 1 trong 4 đáp án trên\n");
+    char dApan;
+    scanf(" %s",&dApan);
+    switch (dApan){
+         case 'a': printf(" đáp án chính xắc!\n");
+         break;
+         case 'b': printf("sai rồi huhu!\n");
+         break;
+         case 'c':printf("sai rồi huhu!\n");
+         break;
+         case 'd':printf("sai rồi hihi!\n");
+    }
+    return 0;
     
-    // Khai báo biến
 
 
     // Nhập dữ liệu
